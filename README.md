66d92089d0035da4898fa236cc44044d9bd9918e
rmweir <ricardo.weir1@gmail.com>
2019-08-14 10:59:13 -0700

    added iterations argument, added readme, added requirements.txt

---
+++ README.md
@@ -0,0 +1,17 @@
+### What it does ###
+Searches Rancher's PR build logs for given string
+
+### Setup ###
+must have python3
+
+pip3 install -r requirements.txt
+
+### Help ###
+python start.py help
+
+### How to use ###
+params: string to look for, newest drone index to start at
+
+options (must be after params): -i: how many logs to go through (default is 250)
+
+example: python3 start.py "someuniquetest failed" 3766 -i 500
--- start.py
+++ start.py
@@ -6,13 +6,14 @@ import time
 baseurl = "https://drone-pr.rancher.io/api/repos/rancher/rancher/builds"
 iterations = 250
 
-lookfor = sys.argv[1]
-pr_index = sys.argv[2]
-
 matchedlogs = []
 
-log_parts = ["/1/1", "/1/2", "/2/1", "/2/2", "/3/1", "/3/2"]
+# use ["/1/1", "/1/2", "/2/1", "/2/2", "/3/1", "/3/2"] to search everything (windows and clone logs)
+log_parts = ["/1/2", "/2/2"]
 
+for index, arg in enumerate(sys.argv):
+    if arg == "-i":
+        iterations = int(sys.argv[index + 1])
 
 def download(url):
     request = requests.get(url=url)  # , headers=headers)
@@ -25,11 +26,14 @@ def isMatch(url):
 
 
 if sys.argv[1] == "help":
-    print("Syntax: python start.py <string to look for> <last pr index>")
-    print("Example: python start.py asdfasdfsdag \"not found error\" 2111")
+    print("Syntax: python start.py <string to look for> <last drone build index>")
+    print("Example: python start.py \"not found error\" 2111")
+    print("\nOptional flags:\n-i (number of logs to search through")
 else:
+    lookfor = sys.argv[1]
+    drone_index = sys.argv[2]
     for i in range(iterations):
-        urls = list(map(lambda x: baseurl + "/" + str((int(pr_index) - i)) + "/logs" + x, log_parts))
+        urls = list(map(lambda x: baseurl + "/" + str((int(drone_index) - i)) + "/logs" + x, log_parts))
         # print(urls)
         matchedlogs += list(filter(lambda x: isMatch(x), urls))
         time.sleep(.1)
