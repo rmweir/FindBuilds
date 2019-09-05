336fce9cba033c441014dac6b5c37fbcf51c95ba
Dan Ramich <danold215@gmail.com>
2019-09-05 16:46:37 -0700

    Add a progress bar
    It was nice before, seeing what it was doing but someone made it do
    nothing, wondering it anything was happening. This corrects that.

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
+import json
 import requests
 import sys
-import json
+from functools import partial
 from multiprocessing.dummy import Pool
 
 baseurl = "https://drone-pr.rancher.io/api/repos/rancher/rancher/builds"
 iterations = 250
-
+current = [0]
 # use ["/1/1", "/1/2", "/2/1", "/2/2", "/3/1", "/3/2"] to search everything
 # (windows and clone logs)
 log_parts = ["/1/2", "/2/2"]
@@ -15,12 +16,42 @@ for index, arg in enumerate(sys.argv):
         iterations = int(sys.argv[index + 1])
 
 
+def update_progress(progress):
+    """Displays or updates a console progress bar
+    Accepts a float between 0 and 1. Any int will be converted to a float.
+    A value under 0 represents a 'halt'.
+    A value at 1 or bigger represents 100%"""
+    barLength = 20  # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rProgress: [{0}] {1}% {2}".format(
+        "#"*block + "-"*(barLength-block), round(progress*100), status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+
 def download(url):
-    request = requests.get(url=url)  # , headers=headers)
-    return json.dumps(request.json())
+    resp = requests.get(url=url)  # , headers=headers)
+    if resp.status_code > 400:
+        return ""
+
+    return json.dumps(resp.json())
 
 
-def isMatch(url):
+def isMatch(url, current):
+    current[0] += 1
+    update_progress(current[0]/(iterations*len(log_parts)))
     logs = download(url)
     if lookfor in logs:
         return url
@@ -42,7 +73,7 @@ for i in range(iterations):
                      str((int(drone_index) - i)) + "/logs" + x, log_parts))
 
 pool = Pool(8)
-found = pool.map(isMatch, urls)
+found = pool.map(partial(isMatch, current=current), urls)
 pool.close()
 pool.join()
 
